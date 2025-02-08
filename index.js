c0082442c9eba98f2922886920a0a7f3a337adec
GardunoAlex <monopolyalex80@gmail.com>
2025-02-08 15:16:45 -0600

    still testing

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", async(req,res) => {
+    res.render("index.html");
+})
